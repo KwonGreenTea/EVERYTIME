56bca53d04776c7f3ae520bbd612cd6a2fd77f79
KwonGreenTea <bnzzzang@gmail.com>
2024-07-12 09:15:48 +0900

    aa

--- Everytime/src/main/java/com/everytime/web/controller/BoardController.java
+++ Everytime/src/main/java/com/everytime/web/controller/BoardController.java
@@ -23,44 +23,26 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 
 public class BoardController {
+   
+   @Autowired
+   private PostService postService; 
+   
+   // post_list.jsp 페이지 호출
+   @GetMapping("/main")
+   public String postList(Model model) {
+      log.info("postList()");
 
-	@Autowired
-<<<<<<< HEAD
-	private PostService postService;
+      // 게시물 목록 조회
+      List<PostVO> postList = postService.getAllPosts(1);
 
-	// post_list.jsp �럹�씠吏� �샇異�
-=======
-	private PostService postService; 
-	
-	// post_list.jsp 페이지 호출
->>>>>>> branch 'master' of https://github.com/KwonGreenTea/EVERYTIME.git
-	@GetMapping("/main")
-	public void main() {
-		log.info("main()");
-	}
+//      PageMaker pageMaker = new PageMaker();
+//      pageMaker.setPagination(pagination);
+//      pageMaker.setTotalCount(boardService.getTotalCount());
 
-	// post_list.jsp �럹�씠吏� �샇異�
-	@GetMapping("/freeBoard")
-	public String postList(Model model) {
-		log.info("postList()");
-
-		// 게시물 목록 조회
-		List<PostVO> postList = postService.getAllPosts(1);
-
-//		PageMaker pageMaker = new PageMaker();
-//		pageMaker.setPagination(pagination);
-//		pageMaker.setTotalCount(boardService.getTotalCount());
-
-//		model.addAttribute("pageMaker", pageMaker);
-<<<<<<< HEAD
-		model.addAttribute("postList", postList); // �겢�씪�씠�뼵�듃濡� 媛앹껜 �쟾�떖
-
-		return "board/post_list";
-=======
-		model.addAttribute("postList", postList); // 스프링 모델 객체에 저장
-		
-		return "board/main";
->>>>>>> branch 'master' of https://github.com/KwonGreenTea/EVERYTIME.git
-	}
+//      model.addAttribute("pageMaker", pageMaker);
+      model.addAttribute("postList", postList); // 스프링 모델 객체에 저장
+      
+      return "board/main";
+   }
 
 }
